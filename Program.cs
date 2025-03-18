bef673c5e7971150d5497931f6a88687c22aae3d
aysunfarhadli <aysunfarhadli4@gmail.com>
2025-03-18 13:03:20 -0700

    program.cs commited

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MBO mbo = new MBO("Asus", "Yenova", 5000, 4000, 8, 2000);
+            mbo.Sale();
+            mbo.ShowFullData();
+
+            Console.WriteLine("Enter düyməsini basın...");
+            Console.ReadLine();
+        }
+    }
+}
